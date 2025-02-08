3f0c60b4cc6359fb449e830b4aa279d96ad88371
Memi <165068591+YHidalgoReuinuity@users.noreply.github.com>
2025-02-08 03:01:02 -0500

    fix: eslint errors

--- app/libs/shared/ui/logo/props.ts
+++
@@ -1,7 +0,0 @@
-export interface LogoProps {
-  title?: string;
-  href?: string;
-  layout?: "horizontal" | "vertical";
-  color?: "primary" | "secondary";
-  size?: number;
-}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <h3 className="text-8xl text-red-500">Hello, world</h3>
+    </div>
+  );
+};
+
+export default App;
