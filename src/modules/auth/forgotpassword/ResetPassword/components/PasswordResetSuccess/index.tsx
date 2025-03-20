65a383cb1d3aa226acf68539829a73b6972439dc
Memi <165068591+YHidalgoReuinuity@users.noreply.github.com>
2025-03-20 15:43:29 -0500

    feature-7-reset-password update: redirect user to login in 5 seconds

--- src/modules/auth/forgotpassword/ResetPassword/components/PasswordResetSuccess/index.tsx
+++ src/modules/auth/forgotpassword/ResetPassword/components/PasswordResetSuccess/index.tsx
@@ -1,10 +1,24 @@
 import { Button } from "@modules/shared/components/buttons/Button";
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { MdExpandCircleDown } from "react-icons/md";
 import { useNavigate } from "react-router";
 
 export const PasswordResetSuccess: FC = () => {
   const navigate = useNavigate();
+
+  const [timer, setTimer] = useState(5);
+
+  useEffect(() => {
+    if (timer !== 0) {
+      setTimeout(() => {
+        setTimer(timer - 1);
+      }, 1000);
+    }
+
+    if (timer === 0) {
+      navigate("/auth/signin");
+    }
+  }, [timer]);
 
   return (
     <div className="flex flex-col items-center justify-center p-4 w-full max-w-lg h-auto bg-white rounded-lg shadow-lg overflow-hidden">
@@ -13,7 +27,7 @@ export const PasswordResetSuccess: FC = () => {
         Password has been reset successfully
       </h1>
       <p className="text-gray-600 my-4 font-secondary">
-        Your password has been change, click below to login
+        You will be redirected to login in {timer} seconds.
       </p>
       <Button onClick={() => navigate("/auth/signin")} label="Back to login" />
     </div>
